5a2c8ea4fa756d7c3f72db1e6847ae2bcf6fa953
John <jnaguib@cmu.edu>
2015-02-22 07:48:30 -0500

    Added the code for ics and fix problems with parsing
    Made the program read the lines from bottom up till it hit the calendar.
    and added the ics file that produces my.ics

--- basic/wordExtraction.py
+++ basic/wordExtraction.py
@@ -1,150 +1,180 @@
 import re
 from datetime import datetime
-import logging
+from ics import Calendar, Event
 
-def ExamString(text, examsList, ExamKeys):
-    flag = False
-    for key in ExamKeys:   # go through the list of the keys
-        if key in text:      # if the key is found in that string
-            examsList = examsList + [text[text.find(key):]]
-            flag = True
-    return (flag, examsList)
+def ExamString (text, examsList, ExamKeys):
+	flag = False
+	for key in ExamKeys:   # go through the list of the keys
+		if key in text:      # if the key is found in that string
+			examsList = examsList + [text[text.find(key):]]
+			flag = True
+	return (flag, examsList)
 
 def AssignmentString(text, assList, AssKeys):
-    flag = False
-    for key in AssKeys:   # go through the list of the keys
-        if key in text:      # if the key is found in that string
-            assList = assList + [text[text.find(key): ]]
-            flag = True
-    return (flag, assList)
+	flag = False
+	for key in AssKeys:   # go through the list of the keys
+		if key in text:      # if the key is found in that string
+			assList = assList + [text[text.find(key): ]]
+			flag = True
+	return (flag, assList)
+
 
 
 
 def textReplacement(text):
-    text = text.replace("january ", " 1/")
-    text = text.replace("jan ", " 1/")
+	text = text.replace("january ", " 1/")
+	text = text.replace("jan ", " 1/")
 
-    text = text.replace("february ", " 2/")
-    text = text.replace("feb ", " 2/")  
+	text = text.replace("february ", " 2/")
+	text = text.replace("feb ", " 2/")	
 
-    text = text.replace("march ", " 3/")
-    text = text.replace("mar ", " 3/")
+	text = text.replace("march ", " 3/")
+	text = text.replace("mar ", " 3/")
 
-    text = text.replace("april ", " 4/")
-    text = text.replace("apr ", " 4/")
+	text = text.replace("april ", " 4/")
+	text = text.replace("apr ", " 4/")
 
-    text = text.replace("may ", " 5/")
+	text = text.replace("may ", " 5/")
 
-    text = text.replace("june ", " 6/")
-    text = text.replace("jun ", " 6/")
+	text = text.replace("june ", " 6/")
+	text = text.replace("jun ", " 6/")
 
-    text = text.replace("july ", " 7/")
+	text = text.replace("july ", " 7/")
 
-    text = text.replace("august ", " 8/")
-    text = text.replace("aug ", " 8/")
+	text = text.replace("august ", " 8/")
+	text = text.replace("aug ", " 8/")
 
-    text = text.replace("september ", " 9/")
-    text = text.replace("sept ", " 9/")
-    text = text.replace("sep ", " 9/")
+	text = text.replace("september ", " 9/")
+	text = text.replace("sept ", " 9/")
+	text = text.replace("sep ", " 9/")
 
-    text = text.replace("october ", " 10/")
-    text = text.replace("oct ", " 10/")
+	text = text.replace("october ", " 10/")
+	text = text.replace("oct ", " 10/")
 
-    text = text.replace("november ", " 11/")
-    text = text.replace("nov ", " 11/")
+	text = text.replace("november ", " 11/")
+	text = text.replace("nov ", " 11/")
 
-    text = text.replace("decemeber ", " 12/")
-    text = text.replace("dec ", " 12/")
-    return text
-
-
-def cleanList(l):
-        l = filter(lambda x: x != "", l)
-        return l
+	text = text.replace("decemeber ", " 12/")
+	text = text.replace("dec ", " 12/")
+	return text
 
 def getDate(text):
-    
-    text = textReplacement(text)
+	
+	text = textReplacement(text)
 
-    
+	match = re.search(r'\d*/\d*', text)
+	year = datetime.now().year
 
-    match = re.search(r'\d*/\d*', text)
-    year = datetime.now().year
+	try:
+		date = datetime.strptime(str(year) + "/" + match.group(), '%Y/%m/%d').date()
+		date = date.strftime("%m/%d/%Y")
+		return (True, date)
+	except:
+		a = 0
 
-    try:
-        date = datetime.strptime(str(year) + "/" + match.group(), '%Y/%m/%d').date()
-        date = date.strftime("%m/%d/%Y")
-        return (True, date)
-    except:
-        a = 0
+	return (False, None)
 
-    return (False, None)
 
+
+
+def createics(assList, examsList):
+	
+	c = Calendar()
+	for i in assList:
+		e = Event()
+		e.name = filter(lambda x: ord(x) < 125, i[1])
+		date = datetime.strptime(i[0], '%m/%d/%Y')
+		e.begin = str(date)
+		#e.end = str(date) + " " + "23:59:59"
+		e.make_all_day()
+		c.events.append(e)
+
+	for i in examsList:
+		e = Event()
+		e.name = filter(lambda x: ord(x) < 125, i[1])
+		date = datetime.strptime(i[0], '%m/%d/%Y')
+		e.begin = str(date)
+		#e.end = str(date) + " " + "23:59:59"
+		e.make_all_day()
+		c.events.append(e)
+
+	with open('my.ics', 'w') as my_file:
+	    my_file.writelines(c)
 
 
 def checkCalendarSection(line, calendarKeys):
-    for i in calendarKeys:
-        if (i in line):
-            return True
-    return False
+	for i in calendarKeys:
+		if (i in line):
+			return True
+	return False
 
 
 
 
 def main(allText):
 
-# MAIN CODE
-#with open ("newsyllabus.txt") as FILE:
-
-    #l = list(FILE)
-    #logging.info(allText)
-    #logging.debug(allText)
-    l = allText.split("\n")
-    # the list to holds the exams and the assignemnts
-    examsList = []
-    assList = []
-
-    # the lists for the keywords of exams and the assignments
-    ExamKeys = ["exam ", "test ", "midterm ", "mid term ", "mid-term ", "quiz "]
-    AssKeys = ["homework ", "assignment ", "assignments ", " problem set ", " problemset ", "lab ", "paper "]
-    CalendarKeys = ["calendar", "schedule", "outline", "calendar:"]
-
-    calendarSection = False; # a flag that we are in the calendarSection
-    dateFound = False  # a flag when the date is found to handle the multi-line cases
-
-    prevDate = None  # initializing the date to None till we encounter a date
-
-    l = cleanList(l)
-    for line in l:
-        # pre-processing for the line
-        line = line.lower().replace("\t", " ")
-      #  line = line + "\n"
-
-        # If we reached to the calendar section set the flag to true
-        if (checkCalendarSection(line, CalendarKeys)):
-            calendarSection = True
-
-        #line = line.replace("\n", "")
+	# MAIN CODE
+	#with open ("Syllabus_ShashankGoyal1.txt") as FILE:
 
 
-        # If we are in the calendar section start getting the information
-        if (calendarSection):
-         
-            (dateFlag, date) = getDate(line)
+		l = allText.split("\n")
 
-            if (dateFlag == True):
-                dateFound = True
-                prevDate = date
-                #line = ' '.join(line2[1:])
-            # examsFlag to check if there was an exams keyword
-            (examsFlag, examsListTemp) = ExamString(line, [], ExamKeys)
-            (assFlag, assListTemp) = AssignmentString(line, [], AssKeys)
+		# the list to holds the exams and the assignemnts
+		examsList = []
+		assList = []
 
-            if (examsFlag == True and prevDate != None):
-                examsList = examsList + [[str(prevDate), examsListTemp[0]]]
-            elif (assFlag == True and prevDate != None):
-                assList = assList + [[str(prevDate), assListTemp[0]]]
+		# the lists for the keywords of exams and the assignments
+		ExamKeys = ["exam", "test", "midterm", "mid term", "mid-term", "quiz"]
+		AssKeys = ["homework", "assignment", "problem set", "problemset", "lab", "paper"]
+		CalendarKeys = ["calendar", "schedule", "course overview"]
 
-    return (assList, examsList)
-     
-        #return ([["abc", "abd"]], [["cdf", "dfs"]])
+		calendarSection = True; # a flag that we are in the calendarSection
+		dateFound = False  # a flag when the date is found to handle the multi-line cases
+
+		prevDate = None  # initializing the date to None till we encounter a date
+
+		for i in range(1, len(l)):
+			line = l[i * -1]
+			# pre-processing for the line
+			line = line.lower()
+			line = line.replace("\t", " ")
+			line = line.replace("\n", "")
+
+			# If we reached to the calendar section set the flag to true
+			if (checkCalendarSection(line, CalendarKeys)):
+				calendarSection = False
+
+			# If we are in the calendar section start getting the information
+			if (calendarSection):
+				
+				(dateFlag, date) = getDate(line)
+
+				if (dateFlag == True):
+					dateFound = True
+					prevDate = date
+
+					#line = ' '.join(line2[1:])
+
+				# examsFlag to check if there was an exams keyword
+				(examsFlag, examsListTemp) = ExamString (line, [], ExamKeys)
+				(assFlag, assListTemp) = AssignmentString(line, [], AssKeys)
+
+				if (examsFlag == True and prevDate != None):
+					examsList = examsList + [(str(prevDate), examsListTemp[0])]
+				elif (assFlag == True and prevDate != None):
+					assList = assList + [(str(prevDate), assListTemp[0])]
+
+		return (assList, examsList)
+
+						
+#a = open("Syllabi/Syllabus_ShashankGoyal3.txt")
+#l = a.read()
+
+#unicode_str = l.decode('utf-8')
+
+#(assList, examsList) = main(l)
+#print (assList)
+#print (examsList)
+
+#createics(assList, examsList)	
+
